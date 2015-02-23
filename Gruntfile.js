9291de074b4b50d390d3b521a205c33b8c90b932
dan <dan.kopparhed@gmail.com>
2015-02-23 20:04:11 +0100

    Adds Gruntfile with tasks to download and unzip chromedriver 2.14 for mac. TODO: select based on OS and latest version. Also, chromedriver requires execute privilege.

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+  // Initial configuration
+  grunt.initConfig({
+    // Download external resources
+    curl: {
+      chromedriver: {
+        src: 'http://chromedriver.storage.googleapis.com/2.14/chromedriver_mac32.zip',
+        dest: 'tmp/chromedriver.zip'
+      }
+    },
+ 
+    // Unzip resources
+    unzip: {
+      chromedriver: {
+        //src: '<config:curl.chromedriver.dest>',
+        src: 'tmp/chromedriver.zip',
+        dest: 'node_modules/.bin/'
+      }
+    }
+  });
+ 
+  // Load in grunt-curl and grunt-zip
+  grunt.loadNpmTasks('grunt-curl');
+  grunt.loadNpmTasks('grunt-zip');
+ 
+  // Create a task to update chromedriver
+  grunt.registerTask('get-chromedriver', 'curl:chromedriver unzip:chromedriver')
+ 
+  // Default task is to update chromedriver
+  grunt.registerTask('default', 'get-chromedriver')
+};
