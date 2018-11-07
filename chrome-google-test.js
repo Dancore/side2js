951470c8d6a9a9a2a33dfedf1cf708bc577fa67f
Dan <dan.kopparhed@gmail.com>
2018-11-07 17:43:41 +0100

    Trying to get the driver to work by config in Windows (10). Finally I "gave up" (for now) and used the env PATH from cmd line trick which works. Then it finds the driver, if installed locally.

--- chrome-google-test.js
+++ chrome-google-test.js
@@ -1,13 +1,28 @@
+/* This seems to work if called with driver in env PATH like this:
+   env PATH="$PATH:./node_modules/.bin/" node chrome-google-test.js */
 var webdriver = require('selenium-webdriver');
 var until = require('selenium-webdriver').until;
+const path = require('path');
+var pchr = path.normalize('selenium-webdriver/chrome');
+console.log("path: " + pchr);
+var chrome = require(pchr);
+// var chrome = require('selenium-webdriver/chrome');
 
 var capabilities = {
   // 'browserName' : 'firefox'
 }
+var thepath = path.normalize(__dirname + '/node_modules/.bin/chromedriver.exe');
+console.log("path: " + thepath);
 
-var chrome = require('selenium-webdriver/chrome');
-var service = new chrome.ServiceBuilder(__dirname + '/node_modules/.bin/chromedriver').build();
-var driver = new chrome.createDriver(capabilities, service);
+// var service = new chrome.ServiceBuilder(thepath).build();
+// var driver = new chrome.createDriver(capabilities, service);
+// var driver = new chrome.Driver();
+var chrome = new webdriver.Builder()
+  .forBrowser('chrome');
+  
+  // ServiceBuilder(thepath);
+// var driver = new webdriver.Builder().forBrowser('chrome').build();
+var driver = chrome.build();
 
 driver.get('http://www.google.com/ncr');
 driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
