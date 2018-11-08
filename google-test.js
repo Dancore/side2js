f649f7eeef7860a21fb3daf658e4c1dd8f656b52
Dan <dan.kopparhed@gmail.com>
2018-11-08 16:37:48 +0100

    Adds screenshot capability. And using the feature to take and save a screenshot of the google search test.

--- google-test.js
+++ google-test.js
@@ -3,6 +3,7 @@
 var webdriver = require('selenium-webdriver');
 var until = require('selenium-webdriver').until;
 const path = require('path');
+const fs = require('fs');
 var edge = require('selenium-webdriver/edge');
 var driverpath = (__dirname + '/node_modules/.bin/MicrosoftWebDriver.exe');
 // driverpath = path.normalize(driverpath); // strangely this test on Edge doesnt work if path is normalized.
@@ -16,10 +17,19 @@ var options = new edge.Options();
 
 var driver = edge.Driver.createSession(options, service);
 
+webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
+  return driver.takeScreenshot().then(function(data) {
+      fs.writeFile(filename, data.replace(/^data:image\/png;base64,/,''), 'base64', function(err) {
+          if(err) throw err;
+      });
+  })
+};
+
 driver.get('https://www.google.com').then(function(){
   driver.findElement(webdriver.By.name('q')).sendKeys('end to end testing\n').then(function(){
     driver.getTitle().then(function(title) {
       console.log('The title is: ' + title);
+      driver.saveScreenshot('screenshot.png');
       driver.quit();
     }).catch(error => { console.log('caught1', error.message); });
   }).catch(error => { console.log('caught2', error.message); });
