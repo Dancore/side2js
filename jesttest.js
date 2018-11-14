0a07aee295b97d8334f3cea5022a2c22374f6772
Dan <dan.kopparhed@gmail.com>
2018-11-14 17:11:37 +0100

    Adds "jesttest" based on the one created by selenium-side-runner.

---
+++ jesttest.js
@@ -0,0 +1,19 @@
+const tests = require("./BBA-test.js");
+global.Key = require('selenium-webdriver').Key;
+global.URL = require('url').URL;
+// global.BASE_URL = configuration.baseUrl || 'https://bbadev.peab.se';
+global.BASE_URL = 'https://bbadev.peab.se';
+let vars = {};
+jest.setTimeout(300000);
+describe("Default Suite", () => {
+  it("Admin-user-attest1", async () => {
+    await tests["Admin-user-attest1"](driver, vars);
+    await driver.getTitle().then(title => {
+      expect(title).toBeDefined();
+    });
+  });
+});
+beforeEach(() => {
+  vars = {};
+});
+afterEach(async () => (cleanup()));
