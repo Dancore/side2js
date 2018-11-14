c021c1f30a25b28386fb06c300b7f8f20b8d8cd0
Dan <dan.kopparhed@gmail.com>
2018-11-14 16:26:48 +0100

    Prep: adds requires. WIP.

--- side2js.js
+++ side2js.js
@@ -1,1 +1,7 @@
 /* Parse Selenium side script and translate to (node) JS. */
+
+const selianize = require('selianize').default;
+const path = require('path');
+const fs = require('fs');
+const beautify = require('js-beautify').js;
+
