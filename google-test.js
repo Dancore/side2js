40755135460063b58d652031d78aceb4049297d7
Dan <dan.kopparhed@gmail.com>
2018-11-14 16:09:44 +0100

    Adds a ";" to avoid issue when parsing.

--- google-test.js
+++ google-test.js
@@ -52,7 +52,7 @@ selianize(obj).then(
         // console.log('selianized test: ' + test.name + ' ' + test.code);
         return (tests += test.code);
       }, 'const tests = {};')
-      .concat('module.exports = tests;');
+      .concat(';module.exports = tests;');
     fs.writeFileSync(obj.name + ".test.js", beautify(tests, { indent_size: 2, space_in_empty_paren: true }));
     code.suites.forEach(suite => {
       console.log('suite: ' + suite.name);
