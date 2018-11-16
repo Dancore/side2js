9a6d138d9f5f48b43f1e55d6cd07d9c694676602
Dan <dan.kopparhed@gmail.com>
2018-11-16 17:06:04 +0100

    Adds feature to read input and output files from CLI. Output path/name optional, otherwise use obj-name in current dir.

--- side2js.js
+++ side2js.js
@@ -4,11 +4,21 @@ const selianize = require('selianize').default;
 const path = require('path');
 const fs = require('fs');
 const beautify = require('js-beautify').js;
+var argv = process.argv;
+var sidefile = argv[2];
+var jsfile = null;
+if (!argv[2]) {
+  console.log('Usage: ' + path.basename(argv[1]) + ' side-script[s] [output-path]');
+  console.warn('Quitting (nothing to do)');
+  process.exit();
+}
 
-var sidefile = "BBA.side";
 // var filedata = fs.readFileSync(sidefile).toString();
 const obj = JSON.parse(fs.readFileSync(sidefile).toString());
 console.log('Side file ' + sidefile + ': ' + obj.id);
+if (argv[3]) jsfile = argv[3];
+else jsfile = obj.name + "-test.js";
+
 selianize(obj).then(
   code => {
     // code.tests.forEach(test => {
