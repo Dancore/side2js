b86b4cc3e4687f10e13ea4afdb87929b6a340646
Dan <dan.kopparhed@gmail.com>
2018-11-16 17:08:31 +0100

    Removed redundant code.

--- side2js.js
+++ side2js.js
@@ -30,21 +30,7 @@ selianize(obj).then(
         return (tests += test.code);
       }, 'const tests = {};')
       .concat(';module.exports = tests;');
-    code.suites.forEach(suite => {
-      console.log('suite: ' + suite.name);
-      // fs.writeFileSync(obj.name + ".test.js", code);
-      if (!suite.tests) {
-        // not parallel
-        console.log('no tests?: ' + suite.name);
-      } else if (suite.tests.length) {
-        // fs.mkdirSync(path.join(projectPath, suite.name));
-        // parallel suite
-        suite.tests.forEach(test => {
-          console.log('selianized test: ' + test.name);
-        });
-      }
-    });
-  }, 
     fs.writeFileSync(jsfile, beautify(tests, { indent_size: 2, space_in_empty_paren: true }));
+  },
   error => { console.log('caught', error.message); } 
 );
