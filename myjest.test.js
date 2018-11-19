5b427de074c0eb9bf419a7a9a8927b5f7ca03861
Dan <dan.kopparhed@gmail.com>
2018-11-19 18:09:58 +0100

    Now using the tests[] and creating an automatic test suite with a simple async await calling of each test, Jest then reports its success or failure. Seems to work, basically, but is still a WIP.

--- myjest.test.js
+++ myjest.test.js
@@ -7,13 +7,31 @@ global.URL = require('url').URL;
 global.BASE_URL = configuration.baseUrl || 'https://bbadev.peab.se';
 let vars = {};
 jest.setTimeout(300000);
-describe("Default Suite", () => {
-  it("Admin-user-attest1", async () => {
-    await tests["Admin-user-attest1"](driver, vars);
-    await driver.getTitle().then(title => {
-      expect(title).toBeDefined();
+
+console.log(Object.entries(tests));
+tests.forEach( obj => { Object.entries(obj).forEach(( [key, fnk]) => { 
+    console.log(`test name: ${key}`); // + '\ntest function:\n' + fnk );
+  });
+});
+// process.exit;
+
+describe("Auto Test Suite", () => {
+  // it("Admin-user-attest1", async () => {
+  //   await tests["Admin-user-attest1"](driver, vars);
+  //   await driver.getTitle().then(title => {
+  //     expect(title).toBeDefined();
+  //   });
+  // });
+  tests.forEach( obj => { Object.entries(obj).forEach(( [key, fnk]) => {
+    it("obj", async () => { 
+      await fnk(driver, vars);
+      await driver.getTitle().then(title => { 
+        expect(title).toBeDefined();
+      });
+    });
     });
   });
+
 });
 beforeEach(() => {
   vars = {};
