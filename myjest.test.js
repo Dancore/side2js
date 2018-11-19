12c41c9465321d94853dd2382ae9e2a31a5d8400
Dan <dan.kopparhed@gmail.com>
2018-11-19 18:05:44 +0100

    Cleaning away some commented code.

--- myjest.test.js
+++ myjest.test.js
@@ -1,7 +1,3 @@
-// const jest = require('jest');
-// var path = require('path');
-// var tmp = "C:\\Apps\\nodejs\\node_modules\\selenium-side-runner\\node_modules\\jest-environment-selenium\\dist\\setup.js"
-// var tmp = "C:\Apps\nodejs\node_modules\selenium-side-runner\node_modules\jest-environment-selenium\dist\setup.js"
 // tmp = path.normalize(tmp);
 // console.log(tmp);
 
@@ -9,7 +5,6 @@ const tests = require("./BBA-test.js");
 global.Key = require('selenium-webdriver').Key;
 global.URL = require('url').URL;
 global.BASE_URL = configuration.baseUrl || 'https://bbadev.peab.se';
-// global.BASE_URL = 'https://bbadev.peab.se';
 let vars = {};
 jest.setTimeout(300000);
 describe("Default Suite", () => {
