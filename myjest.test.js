0f7b4b7f53e35d68122f92a8448d947e8bbde581
Dan <dan.kopparhed@gmail.com>
2018-11-20 14:30:11 +0100

    Cleanup

--- myjest.test.js
+++ myjest.test.js
@@ -1,6 +1,3 @@
-// tmp = path.normalize(tmp);
-// console.log(tmp);
-
 const tests = require("./tests/");
 global.Key = require('selenium-webdriver').Key;
 global.URL = require('url').URL;
@@ -8,20 +5,13 @@ global.BASE_URL = configuration.baseUrl || 'https://bbadev.peab.se';
 let vars = {};
 jest.setTimeout(300000);
 
-console.log(Object.entries(tests));
-tests.forEach( obj => { Object.entries(obj).forEach(( [key, fnk]) => { 
-    console.log(`test name: ${key}`); // + '\ntest function:\n' + fnk );
-  });
-});
-// process.exit;
+// console.log(Object.entries(tests));
+// tests.forEach( obj => { Object.entries(obj).forEach(( [key, fnk]) => { 
+//     console.log(`test name: ${key}`); // + '\ntest function:\n' + fnk );
+//   });
+// });
 
 describe("Auto Test Suite", () => {
-  // it("Admin-user-attest1", async () => {
-  //   await tests["Admin-user-attest1"](driver, vars);
-  //   await driver.getTitle().then(title => {
-  //     expect(title).toBeDefined();
-  //   });
-  // });
   tests.forEach( obj => { Object.entries(obj).forEach(( [key, fnk]) => {
     it("obj", async () => { 
       await fnk(driver, vars);
@@ -31,7 +21,6 @@ describe("Auto Test Suite", () => {
     });
     });
   });
-
 });
 beforeEach(() => {
   vars = {};
